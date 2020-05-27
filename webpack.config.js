7b73d8e31d5293b7981dabee47ceb6a5af9bdfda
Joalbert <joalbertgonzalez@gmail.com>
2020-05-27 11:24:43 -0500

    [ADD]🚀 docker added

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,27 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.ts',
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: [ '.tsx', '.ts', '.js' ]
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+  entry: './src/index.ts',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080
+  }
 };
